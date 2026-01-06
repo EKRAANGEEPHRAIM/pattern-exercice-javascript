cb14c49e002a1dbc412ec8f8d0cf6e37c8d37ad7
Ange Ephraim Ekra <ekraangeephraim@gmail.com>
2026-01-06 12:18:36 +0100

    exercise about duplicates

--- intermediate level/array/arr8.js
+++ intermediate level/array/arr8.js
@@ -1,4 +1,4 @@
-// find every duplicates
+// find each duplicates
 
 function allDuplicates(arr){
     //A Set object allows you to store a set of unique values ​​of any type, whether they are primitive values ​​or objects
