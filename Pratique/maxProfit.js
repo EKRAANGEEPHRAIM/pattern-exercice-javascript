738b4aa02ec31fc9ebe9369ce0ac9e71cdea6257
Ange Ephraim Ekra <ekraangeephraim@gmail.com>
2026-03-17 11:12:41 +0100

    add some exercise

---
+++ Pratique/maxProfit.js
@@ -0,0 +1,27 @@
+// maxprofit
+
+/**
+     * @param {number[]} prices
+     * @return {number}
+     */
+
+const { Result } = require("postcss");
+
+
+function maxProfit(prices){
+    let res = 0 ;
+
+    for(let i = 0 ; i < prices.length ; i++){
+        let buy = prices[i];
+
+        for(let j = i + 1 ; j < prices.length ; j++){
+            let sell = prices[j];
+
+            res = Math.max(res , sell - buy)
+        }
+    }
+
+    return res
+}
+
+console.log(maxProfit([10,1,5,6,7,1]))//6
